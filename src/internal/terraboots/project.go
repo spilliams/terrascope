fdf78f068b232032465bb0d3aba80e2e86922dad
Spencer Williams <s@spencerenglish.com>
2022-12-17 13:25:49 -0800

    validation is hard

--- src/internal/terraboots/project.go
+++ src/internal/terraboots/project.go
@@ -1,89 +1,103 @@
 package terraboots
 
 import (
-	"fmt"
-	"io"
-	"os"
-
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/gohcl"
-	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/function"
+	"github.com/hashicorp/hcl/v2/hclsimple"
 )
+
+type ProjectConfig struct {
+	Project *Project `hcl:"terraboots,block"`
+}
 
 type Project struct {
 	ID       string `hcl:"id,label"`
 	RootsDir string `hcl:"rootsDir"`
 
-	Scopes []*projectScope `hcl:"scope,block"`
+	Scopes []*ProjectScope `hcl:"scope,block"`
 	// roots  []*root
 }
 
-type projectScope struct {
+type ProjectScope struct {
 	Name         string                    `hcl:"name"`
 	Description  string                    `hcl:"description,optional"`
 	DefaultValue string                    `hcl:"default,optional"`
-	Validations  []*projectScopeValidation `hcl:"validation,block"`
+	Validations  []*ProjectScopeValidation `hcl:"validation,block"`
 }
 
-type projectScopeValidation struct {
+type ProjectScopeValidation struct {
 	Condition    bool   `hcl:"condition"`
 	ErrorMessage string `hcl:"error_message"`
 }
 
-var terrabootsSchema = &hcl.BodySchema{
-	Blocks: []hcl.BlockHeaderSchema{
-		{Type: "terraboots", LabelNames: []string{"id"}},
-	},
-}
-
 func ParseProject(filename string) (*Project, error) {
-	parser := hclparse.NewParser()
-	f, diags := parser.ParseHCLFile(filename)
-	if err := handleDiags(diags, parser.Files(), nil); err != nil {
+	cfg := &ProjectConfig{}
+	// TODO: use this for parsing scope validation blocks...
+	// ctx := &hcl.EvalContext{
+	// 	Variables: map[string]cty.Value{
+	// 		"scope": cty.ObjectVal(map[string]cty.Value{
+	// 			"value":
+	// 		})
+	// 	},
+	// 	// shamelessly stolen from Terraform
+	// 	// https://github.com/hashicorp/terraform/blob/f8669d235174ebdaf503d1cd400e22eb51c74c3b/internal/lang/functions.go
+	// 	Functions: map[string]function.Function{
+	// 		"abs":             stdlib.AbsoluteFunc,
+	// 		"can":             tryfunc.CanFunc,
+	// 		"ceil":            stdlib.CeilFunc,
+	// 		"chomp":           stdlib.ChompFunc,
+	// 		"coalescelist":    stdlib.CoalesceListFunc,
+	// 		"compact":         stdlib.CompactFunc,
+	// 		"concat":          stdlib.ConcatFunc,
+	// 		"contains":        stdlib.ContainsFunc,
+	// 		"csvdecode":       stdlib.CSVDecodeFunc,
+	// 		"distinct":        stdlib.DistinctFunc,
+	// 		"element":         stdlib.ElementFunc,
+	// 		"chunklist":       stdlib.ChunklistFunc,
+	// 		"flatten":         stdlib.FlattenFunc,
+	// 		"floor":           stdlib.FloorFunc,
+	// 		"format":          stdlib.FormatFunc,
+	// 		"formatdate":      stdlib.FormatDateFunc,
+	// 		"formatlist":      stdlib.FormatListFunc,
+	// 		"indent":          stdlib.IndentFunc,
+	// 		"join":            stdlib.JoinFunc,
+	// 		"jsondecode":      stdlib.JSONDecodeFunc,
+	// 		"jsonencode":      stdlib.JSONEncodeFunc,
+	// 		"keys":            stdlib.KeysFunc,
+	// 		"log":             stdlib.LogFunc,
+	// 		"lower":           stdlib.LowerFunc,
+	// 		"max":             stdlib.MaxFunc,
+	// 		"merge":           stdlib.MergeFunc,
+	// 		"min":             stdlib.MinFunc,
+	// 		"parseint":        stdlib.ParseIntFunc,
+	// 		"pow":             stdlib.PowFunc,
+	// 		"range":           stdlib.RangeFunc,
+	// 		"regex":           stdlib.RegexFunc,
+	// 		"regexall":        stdlib.RegexAllFunc,
+	// 		"reverse":         stdlib.ReverseListFunc,
+	// 		"setintersection": stdlib.SetIntersectionFunc,
+	// 		"setproduct":      stdlib.SetProductFunc,
+	// 		"setsubtract":     stdlib.SetSubtractFunc,
+	// 		"setunion":        stdlib.SetUnionFunc,
+	// 		"signum":          stdlib.SignumFunc,
+	// 		"slice":           stdlib.SliceFunc,
+	// 		"sort":            stdlib.SortFunc,
+	// 		"split":           stdlib.SplitFunc,
+	// 		"strrev":          stdlib.ReverseFunc,
+	// 		"substr":          stdlib.SubstrFunc,
+	// 		"timeadd":         stdlib.TimeAddFunc,
+	// 		"title":           stdlib.TitleFunc,
+	// 		"trim":            stdlib.TrimFunc,
+	// 		"trimprefix":      stdlib.TrimPrefixFunc,
+	// 		"trimspace":       stdlib.TrimSpaceFunc,
+	// 		"trimsuffix":      stdlib.TrimSuffixFunc,
+	// 		"try":             tryfunc.TryFunc,
+	// 		"upper":           stdlib.UpperFunc,
+	// 		"values":          stdlib.ValuesFunc,
+	// 		"zipmap":          stdlib.ZipmapFunc,
+	// 	},
+	// }
+	err := hclsimple.DecodeFile(filename, nil, cfg)
+	if err != nil {
 		return nil, err
 	}
-
-	project := &Project{}
-	content, diags := f.Body.Content(terrabootsSchema)
-	if err := handleDiags(diags, parser.Files(), nil); err != nil {
-		return nil, err
-	}
-	ctx := &hcl.EvalContext{
-		Variables: map[string]cty.Value{},
-		Functions: map[string]function.Function{},
-	}
-	for _, block := range content.Blocks {
-		if block.Type != "terraboots" {
-			return nil, fmt.Errorf("found unknown block type %s: %+v", block.Type, block)
-		}
-		diags := gohcl.DecodeBody(block.Body, ctx, project)
-		if err := handleDiags(diags, parser.Files(), nil); err != nil {
-			return nil, err
-		}
-		project.ID = block.Labels[0]
-	}
-
-	return project, nil
-}
-
-func handleDiags(diags hcl.Diagnostics, files map[string]*hcl.File, writer io.Writer) error {
-	if diags == nil {
-		return nil
-	}
-	if writer == nil {
-		writer = os.Stderr
-	}
-	if diags.HasErrors() {
-		wr := hcl.NewDiagnosticTextWriter(
-			writer,
-			files,
-			100,   // wrapping width
-			false, // colors
-		)
-		wr.WriteDiagnostics(diags)
-		return fmt.Errorf("diagnostic errors found")
-	}
-	return nil
+	return cfg.Project, nil
 }
