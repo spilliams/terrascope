741c32d21b8e850561f383089825b8d9e30d4863
Spencer Williams <s@spencerenglish.com>
2023-02-22 10:45:10 -0800

    commonly-used flags should have a shorthand

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -61,7 +61,7 @@ func NewTerrascopeCmd() *cobra.Command {
 		Short: "A build orchestrator for terraform monorepos",
 	}
 
-	cmd.PersistentFlags().BoolVar(&dryRun, "dry-run", false, "don't actually execute the task, just print it out")
+	cmd.PersistentFlags().BoolVarP(&dryRun, "dry-run", "d", false, "don't actually execute the task, just print it out")
 	cmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "increase log output")
 	cmd.PersistentFlags().BoolVar(&vertrace, "vvv", false, "increase log output even more")
 	cmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "silences all logs but the errors (and prints those to stderr). Still prints command output to stdout. Overrides verbose and vvv")
