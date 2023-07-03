4f06ffb502dfa83266e69996cdcab445cb004192
Spencer Williams <s@spencerenglish.com>
2023-07-02 22:30:43 -0700

    out of scope

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -10,14 +10,9 @@ import (
 	"github.com/spilliams/terrascope/internal/logformatter"
 )
 
-var dryRun bool
 var quiet bool
 var verbose bool
 var vertrace bool
-
-var all bool
-var yesOne bool
-var noNone bool
 
 var log *logrus.Entry
 
@@ -51,14 +46,9 @@ func NewTerrascopeCmd() *cobra.Command {
 		Short: "A build orchestrator for terraform monorepos",
 	}
 
-	cmd.PersistentFlags().BoolVarP(&dryRun, "dry-run", "d", false, "don't actually execute the task, just print it out")
 	cmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "increase log output")
 	cmd.PersistentFlags().BoolVar(&vertrace, "vvv", false, "increase log output even more")
 	cmd.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "silences all logs but the errors (and prints those to stderr). Still prints command output to stdout. Overrides verbose and vvv")
-
-	cmd.PersistentFlags().BoolVarP(&all, "all", "a", false, "automatically respond to 'yes/no/all' or 'none/one/all' prompts with 'all'. Overrides both yes and no.")
-	cmd.PersistentFlags().BoolVarP(&yesOne, "yes", "y", false, "automatically respond to 'yes/no/all' prompts with 'yes', or 'none/one/all' prompts with 'one'")
-	cmd.PersistentFlags().BoolVarP(&noNone, "no", "n", false, "automatically respond to 'yes/no/all' prompts with 'no', or 'none/one/all' prompts with 'none'. Overrides yes.")
 
 	cmd.AddCommand(newVersionCommand())
 
