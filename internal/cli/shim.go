023ae55960daa2e3437814f5dcc00bcb59ad1f8d
Spencer Williams <s@spencerenglish.com>
2023-07-02 22:59:29 -0700

    don't repeat yourself

--- internal/cli/shim.go
+++ internal/cli/shim.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -18,43 +19,12 @@ func newSpecificTerraformCommand(name string) *cobra.Command {
 
 		PersistentPreRunE: parseProject,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := project.AddAllRoots()
+			tf, err := buildTerraformCommand(args, name)
 			if err != nil {
 				return err
 			}
 
-			// log.Infof("args: %+v", args)
-			scopes := make([]string, 0, len(args)-1)
-			tfargs := make([]string, 0, len(args)-1)
-			tfargs = append(tfargs, name)
-			i := 1
-			for i = 1; i < len(args); i++ {
-				ok, err := project.IsScopeValue(args[i])
-				if err != nil {
-					return err
-				}
-				if ok {
-					scopes = append(scopes, args[i])
-				} else {
-					break
-				}
-			}
-			tfargs = append(tfargs, args[i:]...)
-			log.Infof("found scopes: %+v", scopes)
-			log.Infof("remaining args: %+v", args[i:])
-			// get a list of locations to run in
-			dirs, err := project.BuildRoot(args[0], scopes, dryRun, chainDependenciesOption())
-			if err != nil {
-				return err
-			}
-
-			// TODO: use a worker pool
-			for _, dir := range dirs {
-				err = runTerraform(dir, tfargs, log)
-				if err != nil {
-					return err
-				}
-			}
+			tf.run()
 			return nil
 		},
 	}
@@ -73,42 +43,12 @@ func newGenericTerraformCommand() *cobra.Command {
 
 		PersistentPreRunE: parseProject,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := project.AddAllRoots()
+			tf, err := buildTerraformCommand(args, "")
 			if err != nil {
 				return err
 			}
 
-			// log.Infof("args: %+v", args)
-			scopes := make([]string, 0, len(args)-1)
-			tfargs := make([]string, 0, len(args)-1)
-			i := 1
-			for i = 1; i < len(args); i++ {
-				ok, err := project.IsScopeValue(args[i])
-				if err != nil {
-					return err
-				}
-				if ok {
-					scopes = append(scopes, args[i])
-				} else {
-					break
-				}
-			}
-			tfargs = args[i:]
-			log.Infof("found scopes: %+v", scopes)
-			log.Infof("remaining args: %+v", args[i:])
-			// get a list of locations to run in
-			dirs, err := project.BuildRoot(args[0], scopes, dryRun, chainDependenciesOption())
-			if err != nil {
-				return err
-			}
-
-			// TODO: use a worker pool
-			for _, dir := range dirs {
-				err = runTerraform(dir, tfargs, log)
-				if err != nil {
-					return err
-				}
-			}
+			tf.run()
 			return nil
 		},
 	}
@@ -116,11 +56,67 @@ func newGenericTerraformCommand() *cobra.Command {
 	return cmd
 }
 
-func runTerraform(cwd string, args []string, log *logrus.Entry) error {
+type tfCmd struct {
+	dirs []string
+	args []string
+}
+
+func buildTerraformCommand(args []string, cmd string) (*tfCmd, error) {
+	err := project.AddAllRoots()
+	if err != nil {
+		return nil, err
+	}
+
+	// log.Infof("args: %+v", args)
+	scopes := make([]string, 0, len(args)-1)
+	tfargs := make([]string, 0, len(args)-1)
+	if len(cmd) > 0 {
+		tfargs = append(tfargs, cmd)
+	}
+	i := 1
+	for i = 1; i < len(args); i++ {
+		ok, err := project.IsScopeValue(args[i])
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			scopes = append(scopes, args[i])
+		} else {
+			break
+		}
+	}
+	tfargs = append(tfargs, args[i:]...)
+	log.Infof("found scopes: %+v", scopes)
+	log.Infof("remaining args: %+v", args[i:])
+	// get a list of locations to run in
+	dirs, err := project.BuildRoot(args[0], scopes, dryRun, chainDependenciesOption())
+	if err != nil {
+		return nil, err
+	}
+
+	return &tfCmd{
+		dirs: dirs,
+		args: tfargs,
+	}, nil
+}
+
+func (tf *tfCmd) run() {
+	var wg sync.WaitGroup
+	wg.Add(len(tf.dirs))
+	for _, dir := range tf.dirs {
+		go runTerraform(&wg, dir, tf.args, log)
+	}
+	wg.Wait()
+}
+
+func runTerraform(wg *sync.WaitGroup, cwd string, args []string, log *logrus.Entry) {
 	cmd := shell.NewCommand("terraform", args, cwd, log.Logger)
 	if dryRun {
 		fmt.Printf("%s\n", cmd.String())
-		return nil
+		err := cmd.Run()
+		if err != nil {
+			log.Error(err.Error())
+		}
 	}
-	return cmd.Run()
+	wg.Done()
 }
